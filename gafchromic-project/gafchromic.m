ed95ad86326d4906fe0357894e85f89bff4ce474
cptfinch <cptfinch@gmail.com>
2024-05-15 13:11:19 +0100

    Adding files that need to be merged

---
+++ gafchromic-project/gafchromic.m
@@ -0,0 +1,22 @@
+(* Mathematica Package *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+
+(* :Title: gafchromic *)
+(* :Context: gafchromic` *)
+(* :Author: cptfinch *)
+(* :Date: 2018-01-04 *)
+
+(* :Package Version: 0.1 *)
+(* :Mathematica Version: *)
+(* :Copyright: (c) 2018 cptfinch *)
+(* :Keywords: *)
+(* :Discussion: *)
+
+BeginPackage["gafchromic`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"]
+
+End[] (* `Private` *)
+
+EndPackage[]
